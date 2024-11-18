5b4e983843e5f60ccb303871b66ae5925aa772d1
Dimitrilhostis <103448862+Dimitrilhostis@users.noreply.github.com>
2024-11-18 15:57:30 +0100

    Update

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.querySelector('h1').addEventListener('click', () => {
-    alert('Bienvenue sur mon portfolio !');
-});
+function showDetails(sectionId) {
+    const modal = document.getElementById('details');
+    modal.style.display = 'block';
+
+    const sections = document.querySelectorAll('.detail-section');
+    sections.forEach(section => (section.style.display = 'none'));
+
+    document.getElementById(sectionId).style.display = 'block';
+}
+
+function hideDetails() {
+    const modal = document.getElementById('details');
+    modal.style.display = 'none';
+}
