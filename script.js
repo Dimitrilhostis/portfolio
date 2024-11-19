c1a9277cd7d9b0a1b6c08896abe310629126a692
Dimitrilhostis <103448862+Dimitrilhostis@users.noreply.github.com>
2024-11-19 13:25:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Sélection des éléments HTML
+const introPage = document.getElementById('introPage');
+const mainPage = document.getElementById('mainPage');
+const enterButton = document.getElementById('enterButton');
+
+// Ajout d'un événement au clic sur le bouton
+enterButton.addEventListener('click', () => {
+    // Anime la page d'intro vers le haut
+    introPage.style.transform = 'translateY(-100%)';
+
+    // Anime la page principale pour qu'elle prenne toute la place
+    mainPage.style.transform = 'translateY(-100vh)';
+});
+
+
 // Afficher une page de détail
 function showDetails(sectionId) {
     const modal = document.getElementById('details');
@@ -31,20 +46,18 @@ function hideForm() {
 function toggleRecurringForm(isRecurring) {
     const recurringForm = document.getElementById('recurring-form');
     const nonRecurringForm = document.getElementById('non-recurring-form');
-    const yes = document.getElementById('cb_yes');
-    const no = document.getElementById('cb_no');
+    const yes = document.getElementById('yes');
+    const no = document.getElementById('no');
 
     
     if (isRecurring) {
         recurringForm.style.display = 'grid';
         nonRecurringForm.style.display = 'none';
         no.checked = false
-        yes.checked = true
     } else {
         recurringForm.style.display = 'none';
         nonRecurringForm.style.display = 'block';
         yes.checked = false
-        no.checked = true
     }
 }
 
